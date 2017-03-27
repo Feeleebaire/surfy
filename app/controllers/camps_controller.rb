f9d544ba0c57e7f8fe9a314756b9aa49c16ea8f9
Philibert Latournerie <philibert.latournerie@hotmail.fr>
2017-03-27 21:00:24 +0200

    merde

--- app/controllers/camps_controller.rb
+++ app/controllers/camps_controller.rb
@@ -1,13 +1,3 @@
 class CampsController < ApplicationController
-  def title:string
-  end
 
-  def description:text
-  end
-
-  def language:string
-  end
-
-  def address:string
-  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,4 @@
 Rails.application.routes.draw do
-  get 'camps/title:string'
-
-  get 'camps/description:text'
-
-  get 'camps/language:string'
-
-  get 'camps/address:string'
-
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
