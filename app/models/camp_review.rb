4ea10f84a719110d92bd371df992a43355d0e856
Philibert Latournerie <philibert.latournerie@hotmail.fr>
2017-03-28 15:55:56 +0200

    validation camp review

--- app/models/camp_review.rb
+++ app/models/camp_review.rb
@@ -1,4 +1,9 @@
 class CampReview < ApplicationRecord
+  #associations
   belongs_to :camp
   belongs_to :user
+  #validations
+  validates :rate, presence: true
+  validates :title, presence: true
+  validates :coment, presence: true
 end
