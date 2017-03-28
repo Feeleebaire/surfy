8ff51f13b7d79446d7dac79d20ab54adea44b154
Philibert Latournerie <philibert.latournerie@hotmail.fr>
2017-03-28 18:09:18 +0200

    controller new and create organisation

--- app/controllers/organisations_controller.rb
+++ app/controllers/organisations_controller.rb
@@ -1,3 +1,28 @@
 class OrganisationsController < ApplicationController
 
+  def new
+    @organisation = Organisation.new
+  end
+
+  def create
+    @organisation = Organisation.new(organisation_params)
+    @organisation.save
+    redirect_to organisation_path(@organisation)
+  end
+
+  def show
+    @organisation = Organisation.find(params[:id])
+  end
+
+  def update
+  end
+
+
+  private
+
+  def organisation_params
+    params.require(:organisation).permit(:name, :address)
+  end
+
+
 end
