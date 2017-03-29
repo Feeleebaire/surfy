f548e99760df1a91b2328fe784082a3f00dddba0
Philibert Latournerie <philibert.latournerie@hotmail.fr>
2017-03-29 18:32:02 +0200

    camp controller

--- app/controllers/camps_controller.rb
+++ app/controllers/camps_controller.rb
@@ -1,2 +1,27 @@
 class CampsController < ApplicationController
+
+  def show
+    @camp = Camp.find(params[:id])
+  end
+
+  def new
+    @camp = Camp.new
+  end
+
+  def create
+    @camp = Camp.new(camp_params)
+    @camp.user = current_user
+    if @camp.save
+      redirect_to camps_path(@camp)
+    else
+      render :new
+    end
+  end
+
+
+  private
+
+  def camp_params
+    params.require(:camp).permit(:title, :description, :address, :language)
+  end
 end
